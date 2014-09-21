aca3e3bfac2737659458c7bbbf7f8934327fcd94
Andrew Crerar <asc9003@rit.edu>
2014-09-20 20:30:57 -0400

    Cube: pulled functions out of the API that dont need to be exposed

--- assets/js/lib/cube.js
+++ assets/js/lib/cube.js
@@ -5,55 +5,7 @@ define([
 ], function($) {
   'use strict';
 
-  return {
-    init: function(feedId, stillId) {
-      var video = this._initVideo(feedId, stillId),
-          stats = this._initStats(),
-          scene = new THREE.Scene(),
-          camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
-          renderer = new THREE.WebGLRenderer(),
-          geometry = new THREE.BoxGeometry(300, 300, 300),
-          material = new THREE.MeshLambertMaterial({ map: video.texture }),
-          cube = new THREE.Mesh(geometry, material),
-          ambientLight = new THREE.AmbientLight(0xbbbbbb),
-          directionalLight = new THREE.DirectionalLight(0xffffff);
-
-      cube.overdraw = true;
-      camera.position.z = 500;
-      directionalLight.position.set(1, 1, 1).normalize();
-
-      scene.add(cube);
-      scene.add(ambientLight);
-      scene.add(directionalLight);
-
-      renderer.setSize(window.innerWidth, window.innerHeight);
-
-      $('body').append(renderer.domElement);
-
-      function render() {
-        if (video.feed.readyState === video.feed.HAVE_ENOUGH_DATA) {
-          video.context.drawImage(video.feed, 0, 0, video.still.width, video.still.height);
-
-          if (video.texture) {
-            video.texture.needsUpdate = true;
-          }
-        }
-
-        cube.rotation.x += 0.027;
-        cube.rotation.y += 0.027;
-
-        renderer.render(scene, camera);
-      }
-
-      function animate() {
-        window.requestAnimationFrame(animate);
-        render();
-        stats.update();
-      }
-
-      animate();
-    },
-    _initVideo: function(feedId, stillId) {
+    function initVideo(feedId, stillId) {
       var feed = document.getElementById(feedId),
           still = document.getElementById(stillId),
           context = still.getContext('2d'),
@@ -71,8 +23,9 @@ define([
         context: context,
         texture: texture
       }
-    },
-    _initStats: function() {
+    }
+
+    function initStats() {
       var stats = new Stats();
 
       stats.domElement.style.position = 'absolute';
@@ -83,5 +36,54 @@ define([
 
       return stats;
     }
-  };
+
+    return {
+      init: function(feedId, stillId) {
+        var video = initVideo(feedId, stillId),
+            stats = initStats(),
+            scene = new THREE.Scene(),
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+            renderer = new THREE.WebGLRenderer(),
+            geometry = new THREE.BoxGeometry(300, 300, 300),
+            material = new THREE.MeshLambertMaterial({ map: video.texture }),
+            cube = new THREE.Mesh(geometry, material),
+            ambientLight = new THREE.AmbientLight(0xbbbbbb),
+            directionalLight = new THREE.DirectionalLight(0xffffff);
+
+        cube.overdraw = true;
+        camera.position.z = 500;
+        directionalLight.position.set(1, 1, 1).normalize();
+
+        scene.add(cube);
+        scene.add(ambientLight);
+        scene.add(directionalLight);
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        $('body').append(renderer.domElement);
+
+        function render() {
+          if (video.feed.readyState === video.feed.HAVE_ENOUGH_DATA) {
+            video.context.drawImage(video.feed, 0, 0, video.still.width, video.still.height);
+
+            if (video.texture) {
+              video.texture.needsUpdate = true;
+            }
+          }
+
+          cube.rotation.x += 0.027;
+          cube.rotation.y += 0.027;
+
+          renderer.render(scene, camera);
+        }
+
+        function animate() {
+          window.requestAnimationFrame(animate);
+          render();
+          stats.update();
+        }
+
+        animate();
+      }
+   };
 });
